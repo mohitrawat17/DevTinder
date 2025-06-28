3042717a63038bb570696e693f399f1d0437a46e
mohit17antino <mohit.r@antino.com>
2025-06-28 21:13:19 +0530

    http methods

--- src/app.js
+++ src/app.js
@@ -4,14 +4,30 @@ const app=express()
 const PORT=5000
 
 
+app.get('/test',(req,res)=>{
+    console.log('get request')
+    res.send('test get request')
+})
+
+
+app.post('/test',(req,res)=>{
+    console.log('post request')
+    res.send('test post request')
+})
+
+app.delete('/test',(req,res)=>{
+    console.log('delete request')
+    res.send('test delete request')
+})
+
+
 // request handler
 app.use('/test',(req,res)=>{
-    console.log('test route')
+    console.log('test route middleware')
     res.send('test route')
 })
 
 
-
 app.listen(PORT,()=>{
-    console.log('res')
+    console.log('listening to PORT : ',PORT)
 })
