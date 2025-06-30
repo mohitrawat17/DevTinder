958d274dfeadb63c4c0a90d9f975140c14a35773
mohit17antino <mohit.r@antino.com>
2025-06-30 21:21:07 +0530

    auth middleware

--- src/app.js
+++ src/app.js
@@ -3,41 +3,38 @@ const express= require('express')
 const app=express()
 const PORT=5000
 
+const token='abc'
 
-app.get('/test',(req,res)=>{
+
+app.use((req,res,next)=>{
+ const isAuthorized=token==='abc'
+ if(!isAuthorized){
+    res.status(401).send({'message':'User not authorized'})
+ }
+ else{
+    next()
+ }
+})
+
+
+app.get('/user',(req,res)=>{
     console.log('get request')
-    res.send('test get request')
+    res.send('user get request')
 })
 
-// POST REQUEST WITH QUERY PARAMETERS
-app.post('/test',(req,res)=>{
-    const userId=req.query.userId
-    const page=req.query.page
-    const pageSize=req.query.pageSize
-    console.log('post request',userId,page,pageSize)
-    res.send('test post request')
-})
 
 // POST REQUEST WITH DYNAMIC PARAMETERS
-app.post('/test/:id',(req,res)=>{
+app.post('/user/:id',(req,res)=>{
     const id = req.params.id
     console.log('post request : ',id)
-    res.send('test post request for id : '+id)
+    res.send('user post request for id : '+id)
 })
 
 
-app.delete('/test',(req,res)=>{
+app.delete('/user',(req,res)=>{
     console.log('delete request')
-    res.send('test delete request')
+    res.send('user delete request')
 })
-
-
-// request handler
-app.use('/test',(req,res)=>{
-    console.log('test route middleware')
-    res.send('test route')
-})
-
 
 app.listen(PORT,()=>{
     console.log('listening to PORT : ',PORT)
