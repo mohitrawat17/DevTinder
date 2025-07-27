d71b261e95807b087888502d157e79e028cfcfb1
mohitrawat17 <mr6114671@gmail.com>
2025-07-27 13:29:05 +0530

    jwt integration

--- src/app.js
+++ src/app.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt"); // to encrypt passwords
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const UserModel = require("./models/user");
 const { validateUser } = require("./utils/validations");
 
 const app = express();
 const PORT = 5000;
+const JWT_SECRET_KEY = "rN9v$1kZ@3qT^eFg7*Lz!Bw#XpM8dHsY";
 
-app.use(express.json()); // this middleware parse JSON data coming from server
+app.use(express.json()); // this middleware parses data coming from server into JSON
+app.use(cookieParser()); // this middleware parses cookies from the request.headers and makes them easily accessible via req.cookies
+app.use("/", async (req, res, next) => {
+  // Exclude login and signup paths
+  if (req.path === "/login" || req.path === "/signup") {
+    return next(); // Skip auth for these routes
+  }
+
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Invalid token");
+    }
+    const jwtDecodedData = jwt.verify(token, JWT_SECRET_KEY); // decrypting data from jwt
+    const userId = jwtDecodedData.id;
+    const user = await UserModel.findOne({ _id: userId });
+    if (!user) {
+      throw new Error("No user found.");
+    }
+
+    next();
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
 app.post("/signup", async (req, res) => {
   const userObj = req.body;
@@ -32,33 +59,47 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-
 // user login
 
 app.post("/login", async (req, res) => {
-  const {emailId,password} = req.body;
+  const { emailId, password } = req.body;
   try {
-    const user=await UserModel.findOne({emailId})
-    if(user){
+    const user = await UserModel.findOne({ emailId });
 
+    if (user) {
       // decrypting password
-      const decryptedPassword=await bcrypt.compare(password,user.password)
-      if(decryptedPassword){
-        res.status(200).send({message:'Logged in successfully !'})
+      const decryptedPassword = await bcrypt.compare(password, user.password);
+      if (decryptedPassword) {
+        // if email and password are validated then create a jwt token
+        const token = jwt.sign({ id: user._id }, JWT_SECRET_KEY); //encrypting data to jwt
+        res.cookie("token", token); // to set token in client side (in cookies)
+        res.status(200).send({ message: "Logged in successfully !" });
+      } else {
+        throw new Error("Invalid credentials.");
       }
-      else{
-        throw new Error('Invalid credentials.')
-      }
-    } 
-    else{
+    } else {
       // throw new Error('Invalid Email Id !')   // this is not a ideal way bcz of this we are exposing our db and telling the user that, Is that email is present or not in our DB ?
-       throw new Error('Invalid credentials.')
+      throw new Error("Invalid credentials.");
     }
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
 
+// api to get my profile
+app.get("/profile", async (req, res) => {
+  const { token } = req.cookies;
+  const jwtDecodedData = jwt.verify(token, JWT_SECRET_KEY); // decrypting data from jwt
+  const userId = jwtDecodedData.id;
+  const user = await UserModel.findOne({ _id: userId });
+  const userWithoutPassword = user.toObject();
+  delete userWithoutPassword.password;
+  try {
+    res.status(200).send(userWithoutPassword);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
 // to get all the user in db
 app.get("/users", async (req, res) => {
