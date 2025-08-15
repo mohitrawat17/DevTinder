c4e7976626d9b4618869a8109a1d3a07f388d55f
mohitrawat17 <mr6114671@gmail.com>
2025-08-15 14:42:34 +0530

    change password API

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const UserModel = require("../models/user");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 const router = express.Router();
 
 // api to get my profile
@@ -14,9 +16,9 @@ router.get("/profile", async (req, res) => {
   }
 });
 
-// api to update user by id
-router.patch("/profile/:id", async (req, res) => {
-  const userId = req.params.id;
+// api to update user
+router.patch("/profile/edit", async (req, res) => {
+  const userId = req.user.id;
 
   try {
     if (req.body.password || req.body.emailId) {
@@ -28,7 +30,7 @@ router.patch("/profile/:id", async (req, res) => {
       runValidators: true, //allows validators to run which are in the schema
     });
     if (updatedUser) {
-      res.status(200).send({ message: "User Updated Successfully !" });
+      res.status(200).send({ message: "User updated successfully !" });
     } else {
       res.status(400).send("User not found !");
     }
@@ -60,6 +62,39 @@ router.delete("/user/:id", async (req, res) => {
     }
   } catch (error) {
     res.status(400).send("Internal server error");
+  }
+});
+
+// api to change password
+
+router.patch("/profile/change-password", async (req, res) => {
+  const currentPassword = req.body.currentPassword;
+  const newPassword = req.body.newPassword;
+
+  // check if new password is strong
+  if (!validator.isStrongPassword(newPassword)) {
+    throw new Error("New password is too weak.");
+  }
+
+  try {
+    const isPasswordValid = await req.user.getDecryptedPassword(
+      currentPassword
+    );
+
+    // check if current password is correct
+    if (isPasswordValid) {
+      const newPasswordHash = await bcrypt.hash(newPassword, 10);
+      const updatedUser = await UserModel.findByIdAndUpdate(req.user.id, {
+        password: newPasswordHash,
+      });
+      if (updatedUser) {
+        res.status(200).send({ message: "Password updated successfully." });
+      }
+    } else {
+      throw new Error("Invalid current password.");
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
   }
 });
 
